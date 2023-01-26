cda3f642a30ece86cb5b0334d85a928d0a2c5f97
jesusperez117 <i20221556.09@puebla.tecnm.mx>
2023-01-25 19:39:34 -0600

    codigo

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,109 @@
-const nombreUsuario = prompt("Hola, como te llamas?");
-alert(`Hola, ${nombreUsuario}, ingresa 2 numeros`);
-console.log(nombreUsuario);
+// const nombreUsuario = prompt("Hola, como te llamas?");
+// alert(`Hola, ${nombreUsuario}, ingresa 2 numeros`);
+// console.log(nombreUsuario);
 
-let numero1 = parseInt(prompt("Numero 1:"));
-console.log(numero1);
-let numero2 = parseInt(prompt("Numero 2:"));
-console.log(numero2);
+// let numero1 = parseInt(prompt("Numero 1:"));
+// console.log(numero1);
+// let numero2 = parseInt(prompt("Numero 2:"));
+// console.log(numero2);
 
-if (numero1 == numero2){
-    alert("Ambos numeros son iguales");
-    }
-    else if(numero1 >= numero2){
-    alert(numero1 + " es mayor que " + numero2);    
-    }
-    else{
-        alert(numero2 + " es mayor que " + numero1);
-    }
+// if (numero1 == numero2){
+//     alert("Ambos numeros son iguales");
+//     }
+//     else if(numero1 >= numero2){
+//     alert(numero1 + " es mayor que " + numero2);    
+//     }
+//     else{
+//         alert(numero2 + " es mayor que " + numero1);
+//     }
 
-const cantidad1 = numero1 + numero2;
-console.log(numero1);
-alert("La suma de los 2 numeros es: " + cantidad1);
+// const cantidad1 = numero1 + numero2;
+// console.log(numero1);
+// alert("La suma de los 2 numeros es: " + cantidad1);
 
-let cantidad2 = numero1 - numero2;
-console.log(numero2);
-alert("La resta de los 2 numeros es: " + cantidad2);
+// let cantidad2 = numero1 - numero2;
+// console.log(numero2);
+// alert("La resta de los 2 numeros es: " + cantidad2);
 
+// function suma1 (a,b,c){
+//     let x = a+b+c;
+//     console.log(x);
+// }
+// suma1(4,5,6);
+
+
+// let comp = [7,5]
+// if (comp[0] > comp[1]){
+//     console.log(comp[0]+ " es mayor que "+ comp[1]);
+// }else{
+//     console.log(comp[1]+ " es mayor que "+ comp[0]);
+// }
+
+// let comp1 = [5,6]
+// if (comp1[0] == comp1[1]){
+//     console.log("Son numeros iguales");
+// }else{
+//     console.log("Son numeros diferentes");
+// }
+
+// const gato = {
+//     nombre: "Pepino",
+//     color: "Negro",
+//     edad: "7 meses",
+//     sexo: "Macho",
+//     tamaño: "Mediano",
+// }
+
+// console.log(gato);
+
+// const {nombre} = gato;
+// console.log(nombre);
+
+// const {color} = gato;
+// console.log(color);
+
+// const {edad} = gato;
+// console.log(edad);
+
+// const elementos = [20,"manzana",3, objetos={numero:54,tipo:"gato"}];
+
+// console.log(elementos);
+// console.log(elementos[2]);
+// console.log(elementos[3].numero);
+
+// let edadUsuario = parseInt(prompt("Cuantos años tienes?"));
+// if (edadUsuario >= 18){
+//     console.log("VALIDO")
+// } else{
+//     console.log("NO VALIDO")
+// }
+
+// let celular = [
+//     {marca: "Apple", modelo: "iPhone 14", capacidad: "128gb"},
+//     {marca: "Samsung", modelo: "Z Flip 4", capacidad: "256gb"},
+//     {marca: "Xiaomi", modelo: "Redmi Note", capacidad: "128gb"},
+// ];
+
+// for(i=0; 1<celular.length; i++){
+//     console.log("Marca: " + celular[i].marca,"Modelo: " + celular[i].modelo,"Capacidad: " + celular[i].capacidad);
+// }
+
+// let edadPromo = prompt("Ingresa tu edad");
+
+// switch (edadPromo) {
+//     case "12":
+//         console.log("Debes de ser mayor de edad para participar");
+//         break;
+
+//         case "18":
+//         console.log("Tienes un 15% de descuento en dulceria");
+//         break;
+
+//         case "22":
+//         console.log("Tienes 2x1 en entradas");
+//         break;
+
+//     default:
+//         console.log("No hay promociones para tu edad");
+//         break;
+// }
